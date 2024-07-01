cc49fb165426cdbd074f6e0f0775b248882e4a99
perplx <perplx@users.noreply.github.com>
2024-06-30 23:30:28 -0400

    Support dialect

--- csvh.py
+++ csvh.py
@@ -13,10 +13,19 @@ from typing import Mapping, TextIO
 # global logger
 logger = logging.getLogger("csvh")
 
+DELIMITER_ATTRS = [
+    "delimiter",
+    "lineterminator",
+    "quoting",
+    "quotechar",
+    "escapechar",
+]
+
 
 def log_dialect(log_level: int, dialect: csv.Dialect) -> None:
-    for attr in ["delimiter", "lineterminator", "quotechar"]:
-        logger.log(log_level, "%s = %r", attr, getattr(dialect, attr))
+    logger.log(log_level, "Dialect:")
+    for attr in DELIMITER_ATTRS:  # FIXME add more fields
+        logger.log(log_level, "\t%s = %r", attr, getattr(dialect, attr))
 
 
 def process_csv(
@@ -24,11 +33,12 @@ def process_csv(
         output_file: TextIO,
         keep_prolog: int,
         skip_prolog: int,
-        dialect_name: str,
+        dialect: str | csv.Dialect,
         keep_cols: Mapping[str,str],
     ) -> None:
     """Read CSV data from `input_file`, process, write to `output_file`"""
 
+    # prepare to read from input-file
     logger.debug("reading from file %s", input_file.name)
 
     # skip prolog
@@ -45,18 +55,24 @@ def process_csv(
             line = input_file.readline()
             logger.debug("skipping prolog line %d / %s : %r", i+1, num_prolog_lines, line)
 
+    # determine dialect
 
-    if dialect_name:
-        logger.debug("using dialect: %r", dialect_name)
-        dialect = dialect_name
-    else:
+    logger.debug("dialect in: %r", dialect)
+    if dialect == "sniff":
         start_offset = input_file.tell()
         csv_sniffer = csv.Sniffer()
-        dialect = csv_sniffer.sniff(input_file.read(1024))
+        dialect: csv.Dialect = csv_sniffer.sniff(input_file.read(1024))
         input_file.seek(start_offset)
-
-        logger.debug("detected dialect: %s", dialect)
+    elif isinstance(dialect, str):
+        if dialect in csv.list_dialects():
+            logger.debug("using dialect: %r", dialect)
+        else:
+            logger.error("unrecognized dialect: %r", dialect)
+    elif isinstance(dialect, csv.Dialect):
+        logger.debug("using dialect: %s", dialect)
         log_dialect(logging.DEBUG, dialect)
+    else:
+        raise ValueError(f"incorrect {csv.Dialect} : {dialect}")
 
     # open input-file
     csv_reader = csv.DictReader(input_file, dialect=dialect)
@@ -72,6 +88,7 @@ def process_csv(
         output_columns = input_columns
     logger.debug("writing %d columns: %s", len(output_columns), output_columns)
 
+    # prepare to write to output-file
     logger.debug("writing to file %s", output_file.name)
 
     # write prolog lines that were kept
@@ -86,7 +103,23 @@ def process_csv(
         output_row = dict((k, v) for (k, v) in input_row.items() if k in output_columns)
         csv_writer.writerow(output_row)
 
-    pass
+
+def dialect_args(args: argparse.Namespace) -> csv.Dialect:
+    """"""
+
+    dialect: csv.Dialect = csv.excel()  # default in std lib
+
+    for attr in DELIMITER_ATTRS:
+        value = getattr(args, attr)
+        if value is not None:
+            logger.debug("arg %r: %r", attr, value)
+            setattr(dialect, attr, value)
+
+    logger.debug("dialect: %s", dialect)
+    log_dialect(logging.DEBUG, dialect)
+
+    return dialect
+
 
 def main():
     # define command-line paramerters
@@ -95,10 +128,12 @@ def main():
     file_group.add_argument("input_file", type=argparse.FileType("rt"))
     file_group.add_argument("output_file", type=argparse.FileType("wt"))
 
-    # dialect options
     dialect_group = arg_parser.add_argument_group("dialect")
-    dialect_group.add_argument("--dialect", choices=csv.list_dialects())
+    dialect_name_group = dialect_group.add_mutually_exclusive_group()
+    dialect_name_group.add_argument("--sniff", action="store_true")
+    dialect_name_group.add_argument("--dialect", choices=csv.list_dialects())
     dialect_group.add_argument("--delimiter")
+    dialect_group.add_argument("--lineterminator")
 
     # FIXME separate input, output dialects
     quoting_group = arg_parser.add_argument_group("quoting")
@@ -119,15 +154,22 @@ def main():
     # prepare logging
     logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.NOTSET)
 
-    dialect_names = csv.list_dialects()
-    logger.debug("dialects: %s", dialect_names)
+    if args.sniff:
+        logger.debug("sniff")
+        dialect = "sniff"
+    elif args.dialect:
+        dialect = args.dialect
+        logger.debug("name %s", dialect)
+    else:
+        logger.debug("args")
+        dialect = dialect_args(args)
 
     process_csv(
         args.input_file,
         args.output_file,
         args.keep_prolog,
         args.skip_prolog,
-        args.dialect,
+        dialect,
         args.keep_cols,
     )
 
