a6187c01b715cfea0f620974a27f4c3ffea7c114
perplx <perplx@users.noreply.github.com>
2024-06-30 21:04:13 -0400

    Added column-name filtering

--- csvh.py
+++ csvh.py
@@ -4,18 +4,63 @@
 
 # standard imports
 import argparse
+import csv
+import logging
+from typing import Mapping, TextIO
+
+
+logger = logging.getLogger("csvh")
+
+
+def process_csv(input_file: TextIO, output_file: TextIO, keep_cols: Mapping[str,str]) -> None:
+    """Read CSV data from `input_file`, process, write to `output_file`"""
+
+    # open input-file
+    logger.debug("reading from file %s", input_file.name)
+    csv_reader = csv.DictReader(input_file)
+    input_columns = csv_reader.fieldnames
+    logger.debug("reading %d columns: %s", len(input_columns), input_columns)
+
+    # if `--keep-cols` was given, filter the column-names
+    if keep_cols:
+        output_columns = [c for c in keep_cols]
+    else:
+        output_columns = input_columns
+    logger.debug("writing %d columns: %s", len(output_columns), output_columns)
+
+    # read from input-file, write to output-file
+    logger.debug("writing to file %s", output_file.name)
+    csv_writer = csv.DictWriter(output_file, output_columns)
+    csv_writer.writeheader()
+    for input_row in csv_reader:
+        output_row = dict((k, v) for (k, v) in input_row.items() if k in output_columns)
+        csv_writer.writerow(output_row)
+
+    pass
 
 def main():
+    # define command-line paramerters
     arg_parser = argparse.ArgumentParser(description=__doc__)
-    arg_parser.add_argument("input_file", type=argparse.FileType("rt"))
-    arg_parser.add_argument("output_file", type=argparse.FileType("wt"))
+    file_group = arg_parser.add_argument_group("file")
+    file_group.add_argument("input_file", type=argparse.FileType("rt"))
+    file_group.add_argument("output_file", type=argparse.FileType("wt"))
 
+    # FIXME separate input, output dialects
     quoting_group = arg_parser.add_argument_group("quoting")
     quoting_group.add_argument("--quoting")
     quoting_group.add_argument("--quotechar")
     quoting_group.add_argument("--escapechar")
 
+    filter_group = arg_parser.add_argument_group("filter")
+    filter_group.add_argument("--keep-cols", type=str, nargs="+", help="list of column-names to keep, in the order to be kept")
+
+    # read command-line paramerters
     args = arg_parser.parse_args()
+
+    # prepare logging
+    logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.NOTSET)
+
+    process_csv(args.input_file, args.output_file, args.keep_cols)
 
 
 if __name__ == "__main__":
