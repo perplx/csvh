67bdf98cb65d169a860c3b0f29f54445f2e22862
perplx <perplx@users.noreply.github.com>
2024-06-30 21:57:14 -0400

    Detect dialect

--- csvh.py
+++ csvh.py
@@ -10,10 +10,23 @@ import logging
 from typing import Mapping, TextIO
 
 
+# global logger
 logger = logging.getLogger("csvh")
 
 
-def process_csv(input_file: TextIO, output_file: TextIO, keep_prolog: int, skip_prolog: int, keep_cols: Mapping[str,str]) -> None:
+def log_dialect(log_level: int, dialect: csv.Dialect) -> None:
+    for attr in ["delimiter", "lineterminator", "quotechar"]:
+        logger.log(log_level, "%s = %r", attr, getattr(dialect, attr))
+
+
+def process_csv(
+        input_file: TextIO,
+        output_file: TextIO,
+        keep_prolog: int,
+        skip_prolog: int,
+        dialect_name: str,
+        keep_cols: Mapping[str,str],
+    ) -> None:
     """Read CSV data from `input_file`, process, write to `output_file`"""
 
     logger.debug("reading from file %s", input_file.name)
@@ -23,17 +36,30 @@ def process_csv(input_file: TextIO, output_file: TextIO, keep_prolog: int, skip_
     if keep_prolog or skip_prolog:
         prolog_lines = []
         for i in range(num_prolog_lines):
-            line = next(input_file)
+            line = input_file.readline()
             logger.debug("keeping prolog line %d / %s : %r", i+1, num_prolog_lines, line)
             if keep_prolog:
                 prolog_lines.append(line)
     elif skip_prolog:
         for i in range(num_prolog_lines):
-            line = next(input_file)
+            line = input_file.readline()
             logger.debug("skipping prolog line %d / %s : %r", i+1, num_prolog_lines, line)
-    
+
+
+    if dialect_name:
+        logger.debug("using dialect: %r", dialect_name)
+        dialect = dialect_name
+    else:
+        start_offset = input_file.tell()
+        csv_sniffer = csv.Sniffer()
+        dialect = csv_sniffer.sniff(input_file.read(1024))
+        input_file.seek(start_offset)
+
+        logger.debug("detected dialect: %s", dialect)
+        log_dialect(logging.DEBUG, dialect)
+
     # open input-file
-    csv_reader = csv.DictReader(input_file)
+    csv_reader = csv.DictReader(input_file, dialect=dialect)
 
     # read column-names
     input_columns = csv_reader.fieldnames
@@ -54,7 +80,7 @@ def process_csv(input_file: TextIO, output_file: TextIO, keep_prolog: int, skip_
         output_file.write(line)
 
     # read from input-file, write to output-file
-    csv_writer = csv.DictWriter(output_file, output_columns)
+    csv_writer = csv.DictWriter(output_file, output_columns, dialect=dialect)
     csv_writer.writeheader()
     for input_row in csv_reader:
         output_row = dict((k, v) for (k, v) in input_row.items() if k in output_columns)
@@ -68,6 +94,11 @@ def main():
     file_group = arg_parser.add_argument_group("file")
     file_group.add_argument("input_file", type=argparse.FileType("rt"))
     file_group.add_argument("output_file", type=argparse.FileType("wt"))
+
+    # dialect options
+    dialect_group = arg_parser.add_argument_group("dialect")
+    dialect_group.add_argument("--dialect", choices=csv.list_dialects())
+    dialect_group.add_argument("--delimiter")
 
     # FIXME separate input, output dialects
     quoting_group = arg_parser.add_argument_group("quoting")
@@ -88,7 +119,17 @@ def main():
     # prepare logging
     logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.NOTSET)
 
-    process_csv(args.input_file, args.output_file, args.keep_prolog, args.skip_prolog, args.keep_cols)
+    dialect_names = csv.list_dialects()
+    logger.debug("dialects: %s", dialect_names)
+
+    process_csv(
+        args.input_file,
+        args.output_file,
+        args.keep_prolog,
+        args.skip_prolog,
+        args.dialect,
+        args.keep_cols,
+    )
 
 
 if __name__ == "__main__":
