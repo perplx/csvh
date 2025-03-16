0db4c86c9de358e70fc90f94752660aaa0f932d7
perplx <perplx@users.noreply.github.com>
2025-03-15 21:46:09 -0400

    Starting from scratch

---
+++ csv2.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+
+import argparse
+import csv
+import logging
+from typing import Iterable, Sequence, TextIO
+
+
+# prepare global logger
+logger = logging.getLogger(__name__)
+
+
+# prolog-filtering
+
+
+def filter_prolog(input_file: TextIO, keep_prolog: int, skip_prolog: int) -> list[str]:
+    """FIXME"""
+
+    prolog_lines = []
+    num_prolog_lines = max(keep_prolog, skip_prolog)
+    for i in range(1, num_prolog_lines + 1):
+        line = input_file.readline()
+        if keep_prolog:
+            logger.debug("keeping prolog line %d / %d : %r", i, num_prolog_lines, line)
+            prolog_lines.append(line)
+        elif skip_prolog:
+            logger.debug("skipping prolog line %d / %d : %r", i, num_prolog_lines, line)
+    return prolog_lines
+
+
+# column-filtering
+
+
+# TODO order by keep_cols? repeats?
+def keep_fields(input_cols: Sequence[str], keep_cols: list[str], skip_cols: list[str]) -> list[str]:
+    """FIXME"""
+
+    kept_cols = list(input_cols)
+    if keep_cols:
+        kept_cols = [c for c in kept_cols if c in keep_cols]
+    if skip_cols:
+        kept_cols = [c for c in kept_cols if c not in skip_cols]
+
+    return kept_cols
+
+
+# fixme modify input row instead?
+def filter_cols(row: dict[str, str], keep_cols: list[str]) -> dict[str, str]:
+    """FIXME"""
+    if keep_cols:
+        return {c: row[c] for c in keep_cols}
+    return row
+
+
+# row-filtering
+
+
+def read_row_filters(row_args: list[list[str]]) -> dict[str, list[str]]:
+    return {r[0]: r[1:] for r in row_args}
+
+
+def keep_row(row, keep_rows: dict[str, list[str]]) -> bool:
+    if keep_rows:
+        for col, values in keep_rows.items():
+            if row[col] not in values:
+                return False
+    return True
+
+
+def skip_row(row, skip_rows: dict[str, list[str]]) -> bool:
+    if skip_rows:
+        for col, values in skip_rows.items():
+            if row[col] in values:
+                return False
+    return True
+
+
+def filter_rows(input_rows: Iterable[dict[str, str]], keep_rows: dict[str, list[str]], skip_rows: dict[str, list[str]]):
+    for row in input_rows:
+        if keep_row(row, keep_rows) and skip_row(row, skip_rows):
+            yield row
+
+
+# csv-processing
+
+
+def process_csv(
+    input_file: TextIO,
+    output_file: TextIO,
+    keep_prolog: int,
+    skip_prolog: int,
+    keep_cols: list[str],
+    skip_cols: list[str],
+    keep_rows: dict[str, list[str]],
+    skip_rows: dict[str, list[str]],
+) -> None:
+    """FIXME"""
+
+    # prepare to read from to input-file
+    logger.debug("reading from file %s", input_file.name)
+
+    # filter prolog-lines
+    prolog_lines = filter_prolog(input_file, keep_prolog, skip_prolog)
+
+    csv_reader = csv.DictReader(input_file)
+    csv_fields = csv_reader.fieldnames
+    if csv_fields is None:
+        csv_fields = []
+
+    # which fields to keep
+    kept_cols = keep_fields(csv_fields, keep_cols, skip_cols)
+    logger.debug("keeping columns: %s", kept_cols)
+
+    # prepare to write to output-file
+    logger.debug("writing to file %s", output_file.name)
+    csv_writer = csv.DictWriter(output_file, kept_cols)
+
+    # write prolog lines that were kept
+    for i, line in enumerate(prolog_lines, start=1):
+        logger.debug("writing prolog line %d / %d : %r", i, len(prolog_lines), line)
+        output_file.write(line)
+
+    # write filtered rows
+    csv_writer.writeheader()
+    for row in filter_rows(csv_reader, keep_rows, skip_rows):
+        row = filter_cols(row, kept_cols)
+        csv_writer.writerow(row)
+
+
+# main
+
+
+def parse_args():
+    """Specify command-line parameters"""
+
+    # define command-line paramerters
+    arg_parser = argparse.ArgumentParser(description=__doc__)
+    file_group = arg_parser.add_argument_group("file")
+    file_group.add_argument("input_file", type=argparse.FileType("rt"))
+    file_group.add_argument("output_file", type=argparse.FileType("wt"))
+
+    # FIXME separate input, output dialects
+    quoting_group = arg_parser.add_argument_group("quoting")
+    quoting_group.add_argument("--quoting")
+    quoting_group.add_argument("--quotechar")
+    quoting_group.add_argument("--escapechar")
+
+    prolog_group = arg_parser.add_mutually_exclusive_group()
+    prolog_group.add_argument(
+        "--keep-prolog", type=int, default=0,
+        help="number of header lines to skip, they will be reproduced in the output"
+    )
+    prolog_group.add_argument(
+        "--skip-prolog", type=int, default=0,
+        help="number of header lines to skip, they will be omitted in the output"
+    )
+
+    filter_group = arg_parser.add_argument_group("filter")
+    filter_group.add_argument(
+        "--keep-cols", type=str, nargs="+", help="list of column-names to keep, in the order to be kept"
+    )
+    filter_group.add_argument(
+        "--skip-cols", type=str, nargs="+", help="list of column-names to skip, in the order to be kept"
+    )
+    filter_group.add_argument("--keep-rows", type=str, nargs="+", action="append", default=[])
+    filter_group.add_argument("--skip-rows", type=str, nargs="+", action="append", default=[])
+
+    # read command-line paramerters
+    args = arg_parser.parse_args()
+    return args
+
+
+def main():
+    # read command-line parameters
+    args = parse_args()
+
+    # prepare logging
+    logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.NOTSET)
+    logger.debug("args: %s", args)
+
+    keep_rows = read_row_filters(args.keep_rows),
+    skip_rows = read_row_filters(args.skip_rows),
+
+    # process CSV
+    process_csv(
+        # files
+        args.input_file,
+        args.output_file,
+        # prolog
+        args.keep_prolog,
+        args.skip_prolog,
+        # filer columns
+        args.keep_cols,
+        args.skip_cols,
+        # filter rows
+        keep_rows,
+        skip_rows,
+    )
+
+
+if __name__ == "__main__":
+    main()
